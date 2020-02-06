d9c14a41885182186e7c309855f4affa6334fa2c
Osama Alvi <osamaalvie@gmail.com>
2020-02-06 12:47:11 +0400

    master

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'candidate'),
             'password' => env('DB_PASSWORD', 'PrototypeRocks123654'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
